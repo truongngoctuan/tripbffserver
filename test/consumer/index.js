6591de29936bbb6b2089dadfe745720ed9f21505
TRUONG Ngoc Tuan <tntuan0712494@gmail.com>
2018-12-18 07:42:52 +0700

    add simple sub sub using redis

---
+++ test/consumer/index.js
@@ -0,0 +1,61 @@
+const redisStore = {
+  // host: "localhost",
+  // port: 6379,
+  host: "127.0.1.1",
+  port: 6379,
+  secret: "asd"
+};
+
+const qName = "myqueue";
+
+const RedisSMQ = require("rsmq");
+const rsmq = new RedisSMQ({
+  host: redisStore.host,
+  port: redisStore.post,
+  ns: "rsmq"
+});
+
+var counter = 0;
+function receiveMessage() {
+  rsmq.receiveMessage({ qname: qName }, function(err, resp) {
+    if (err) {
+      errorHandler(err);
+      SetTimeoutReceiveMessage();
+      return;
+    }
+    if (resp.id) {
+      console.log("Message received.", resp);
+      counter++;
+      console.log("COUNTER", counter);
+      deleteMessage(resp.id);
+    } else {
+      // console.log("no message received");
+      SetTimeoutReceiveMessage();
+    }
+  });
+}
+
+function deleteMessage(messageId) {
+  rsmq.deleteMessage({ qname: "myqueue", id: messageId }, function(err, resp) {
+    if (resp === 1) {
+      console.log("Message deleted.");
+      SetTimeoutReceiveMessage();
+    } else {
+      console.log("Message not found.");
+    }
+  });
+}
+
+function SetTimeoutReceiveMessage() {
+  setTimeout(() => {
+    receiveMessage();
+  }, 50);
+}
+
+function errorHandler(err) {
+  if (err && err.queueNotFound) console.log("ERR", "queueNotFound");
+  else if (err) console.log("ERR", "error on queue");
+}
+
+receiveMessage();
+return;
