a6b35a803b39a23f5ed26f890594b49647777500
TRUONG Ngoc Tuan <tntuan0712494@gmail.com>
2018-12-20 11:08:39 +0700

    cleanup; test queue with multiple publish

--- InfogGraphic/index.js
+++ InfogGraphic/index.js
@@ -1,67 +1,9 @@
-const exporter = require("./chrome-generator")
-const redisStore = {
-  // host: "localhost",
-  // port: 6379,
-  host: "127.0.1.1",
-  port: 6379,
-  secret: "asd"
-};
-
-const qName = "myqueue";
-
-const RedisSMQ = require("rsmq");
-const rsmq = new RedisSMQ({
-  host: redisStore.host,
-  port: redisStore.post,
-  ns: "rsmq"
-});
-
-var counter = 0;
-function receiveMessage() {
-  rsmq.receiveMessage({ qname: qName }, async function(err, resp) {
-    if (err) {
-      errorHandler(err);
-      SetTimeoutReceiveMessage();
-      return;
-    }
-    if (resp.id) {
-      console.log("Message received.", resp);
-      counter++;
-      console.log("COUNTER", counter);
-      await actionExecAsync();
-      deleteMessage(resp.id);
-    } else {
-      // console.log("no message received");
-      SetTimeoutReceiveMessage();
-    }
-  });
-}
-
-function deleteMessage(messageId) {
-  rsmq.deleteMessage({ qname: qName, id: messageId }, function(err, resp) {
-    if (resp === 1) {
-      console.log("Message deleted.");
-      SetTimeoutReceiveMessage();
-    } else {
-      console.log("Message not found.");
-    }
-  });
-}
-
-function SetTimeoutReceiveMessage() {
-  setTimeout(() => {
-    receiveMessage();
-  }, 50);
-}
-
-function errorHandler(err) {
-  if (err && err.queueNotFound) console.log("ERR", "queueNotFound");
-  else if (err) console.log("ERR", "error on queue");
-}
+const exporter = require("./chrome-generator");
+const consumer = require("./_services/consumer-worker");
 
 async function actionExecAsync() {
   await exporter.exportInfo();
 }
 
-receiveMessage();
+consumer.receiveMessage(actionExecAsync);
 return;
