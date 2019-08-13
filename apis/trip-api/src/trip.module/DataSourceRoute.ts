a237fecabf81c8edd9366bb350a495b5ad4da498
Reborn <vubao1690@gmail.com>
2019-08-13 10:13:36 +0700

    Add method to get signed url of icons

--- apis/trip-api/src/trip.module/DataSourceRoute.ts
+++ apis/trip-api/src/trip.module/DataSourceRoute.ts
@@ -50,7 +50,14 @@ module.exports = {
         method: "GET",
         path: "/trips/feelings",
         handler: async function(request) {
-          var feelings = dataSourceQueryHandler.getFeelings();
+          var feelings = await dataSourceQueryHandler.getFeelings();
+
+          for (var i = 0; i < feelings.length; i++) {
+            let item = feelings[i];
+            let signedUrl = item.icon ? await IoC.fileService.signGetIcon(item.icon) : "";
+            item.icon = signedUrl;
+          }
+
           return feelings;
         },
         options: {
@@ -70,247 +77,247 @@ module.exports = {
             feelingId: uuid4(),
             label_en: "Beautiful",
             label_vi: "Đẹp",
-            icon: ""
+            icon: "emoji/01_Beautiful.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Wonderful",
             label_vi: "Tuyệt vời",
-            icon: ""
+            icon: "emoji/02_Wonderful.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Peaceful",
             label_vi: "Yên bình",
-            icon: ""
+            icon: "emoji/03_Peaceful.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Free",
             label_vi: "Tự do",
-            icon: ""
+            icon: "emoji/04_Free.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Comfortable",
             label_vi: "Thoải mái",
-            icon: ""
+            icon: "emoji/05_Comfortable.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Chill",
             label_vi: "Chill",
-            icon: ""
+            icon: "emoji/chill.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Chất phát ngất",
             label_vi: "Chất phát ngất",
-            icon: ""
+            icon: "emoji/Chat_phat_ngat.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Hạn hán lời",
             label_vi: "Hạn hán lời",
-            icon: ""
+            icon: "emoji/han_han_loi.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Mình thích thì mình làm thôi",
             label_vi: "Mình thích thì mình làm thôi",
-            icon: ""
+            icon: "emoji/minh_thich_thi_minh_lam_thoi.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Trẻ trâu",
             label_vi: "Trẻ trâu",
-            icon: ""
+            icon: "emoji/tre_trau.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Cool",
             label_vi: "Ngầu",
-            icon: ""
+            icon: "emoji/cool.png"
           },         
           {
             feelingId: uuid4(),
             label_en: "Happy",
             label_vi: "Hạnh phúc",
-            icon: ""
+            icon: "emoji/happy.png"
           },
           {
             feelingId: uuid4(),
             label_en: "In Love",
             label_vi: "Đang yêu",
-            icon: ""
+            icon: "emoji/in-love.png"
           },      
           {
             feelingId: uuid4(),
             label_en: "Loved",
             label_vi: "Được yêu",
-            icon: ""
+            icon: "emoji/loved.png"
           },   
           {
             feelingId: uuid4(),
             label_en: "Cute",
             label_vi: "Dễ thương",
-            icon: ""
+            icon: "emoji/cute.png"
           },  
           {
             feelingId: uuid4(),
             label_en: "Excited",
             label_vi: "Hào hứng",
-            icon: ""
+            icon: "emoji/excited.png"
           },     
           {
             feelingId: uuid4(),
             label_en: "Crazy",
             label_vi: "Khùng",
-            icon: ""
+            icon: "emoji/crazy.png"
           },  
           {
             feelingId: uuid4(),
             label_en: "Silly",
             label_vi: "Ngớ ngẩn",
-            icon: ""
+            icon: "emoji/silly.png"
           }, 
           {
             feelingId: uuid4(),
             label_en: "Relaxed",
             label_vi: "Thư giãn",
-            icon: ""
+            icon: "emoji/relaxed.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Motivated",
             label_vi: "Có động lực",
-            icon: ""
+            icon: "emoji/motivated.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Confident",
             label_vi: "Tự tin",
-            icon: ""
+            icon: "emoji/confident.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Fresh",
             label_vi: "Tươi mới",
-            icon: ""
+            icon: "emoji/fresh.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Lucky",
             label_vi: "May mắn",
-            icon: ""
+            icon: "emoji/lucky.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Cold",
             label_vi: "Lạnh",
-            icon: ""
+            icon: "emoji/cold.png"
           },    
           {
             feelingId: uuid4(),
             label_en: "Worried",
             label_vi: "Lo lắng",
-            icon: ""
+            icon: "emoji/worried.png"
           },   
           {
             feelingId: uuid4(),
             label_en: "Funny",
             label_vi: "Hài hước",
-            icon: ""
+            icon: "emoji/funny.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Inspired",
             label_vi: "Được truyền cảm hứng",
-            icon: ""
+            icon: "emoji/inspired.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Super",
             label_vi: "Phi thường",
-            icon: ""
+            icon: "emoji/super.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Satisfied",
             label_vi: "Thỏa mãn",
-            icon: ""
+            icon: "emoji/satisfied.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Safe",
             label_vi: "An toàn",
-            icon: ""
+            icon: "emoji/safe.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Sad",
             label_vi: "Buồn",
-            icon: ""
+            icon: "emoji/sad.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Tired",
             label_vi: "Mệt mỏi",
-            icon: ""
+            icon: "emoji/tired.png"
           }, 
           {
             feelingId: uuid4(),
             label_en: "Angry",
             label_vi: "Giận dữ",
-            icon: ""
+            icon: "emoji/angry.png"
           },  
           {
             feelingId: uuid4(),
             label_en: "Exhausted",
             label_vi: "Kiệt sức",
-            icon: ""
+            icon: "emoji/exhausted.png"
           }, 
           {
             feelingId: uuid4(),
             label_en: "Bored",
             label_vi: "Chán",
-            icon: ""
+            icon: "emoji/bored.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Sleepy",
             label_vi: "Buồn ngủ",
-            icon: ""
+            icon: "emoji/sleepy.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Hungry",
             label_vi: "Đói",
-            icon: ""
+            icon: "emoji/hungry.png"
           },
           {
             feelingId: uuid4(),
             label_en: "Bad",
             label_vi: "Tồi tệ",
-            icon: ""
+            icon: "emoji/bad.png"
           }, 
           {
             feelingId: uuid4(),
             label_en: "Disappointed",
             label_vi: "Thất vọng",
-            icon: ""
+            icon: "emoji/disappointed.png"
           },       
           {
             feelingId: uuid4(),
             label_en: "Heartbroken",
             label_vi: "Trái tim tan vỡ",
-            icon: ""
+            icon: "emoji/heartbroken.png"
           },         
           {
             feelingId: uuid4(),
             label_en: "Lonely",
             label_vi: "Cô đơn",
-            icon: ""
+            icon: "emoji/lonely.png"
           }
         ];
         feelingRepo.insertMany(feelings);
@@ -325,7 +332,14 @@ module.exports = {
       method: "GET",
       path: "/trips/activities",
       handler: async function(request) {
-        var activities = dataSourceQueryHandler.getActivities();
+        var activities = await dataSourceQueryHandler.getActivities();
+
+        for (var i = 0; i < activities.length; i++) {
+          let item = activities[i];
+          let signedUrl = item.icon ? await IoC.fileService.signGetIcon(item.icon) : "";
+          item.icon = signedUrl;
+        }
+
         return activities;
       },
       options: {
@@ -345,163 +359,163 @@ module.exports = {
             activityId: uuid4(),
             label_en: "Eat & Drink",
             label_vi: "Ăn uống",
-            icon: ""
+            icon: "activity/Eat.png"
           },
           {
             activityId: uuid4(),
             label_en: "Hiking",
             label_vi: "Đi bộ đường dài ngắn ngày",
-            icon: ""
+            icon: "activity/hiking.png"
           },
           {
             activityId: uuid4(),
             label_en: "Trekking",
             label_vi: "Đi bộ đường dài nhiều ngày",
-            icon: ""
+            icon: "activity/trekking.png"
           },
           {
             activityId: uuid4(),
             label_en: "Walking with a Storm",
             label_vi: "Bước đi với một cơn bão",
-            icon: ""
+            icon: "activity/walking-with-a-storm.png"
           },
           {
             activityId: uuid4(),
             label_en: "Stargazing",
             label_vi: "Ngắm sao",
-            icon: ""
+            icon: "activity/stargazing.png"
           },
           {
             activityId: uuid4(),
             label_en: "Cloud hunting",
             label_vi: "Săn mây",
-            icon: ""
+            icon: "activity/cloud.png"
           },
           {
             activityId: uuid4(),
             label_en: "Watch the sunrise",
             label_vi: "Ngắm mặt trời mọc",
-            icon: ""
+            icon: "activity/sunrise.png"
           },
           {
             activityId: uuid4(),
             label_en: "Watch the sunset",
             label_vi: "Ngắm mặt trời lặn",
-            icon: ""
+            icon: "activity/sunset.png"
           },
           {
             activityId: uuid4(),
             label_en: "See rice fields",
             label_vi: "Ngắm ruộng lúa",
-            icon: ""
+            icon: "activity/rice_fields.png"
           },
           {
             activityId: uuid4(),
             label_en: "Rock climbing",
             label_vi: "Leo núi",
-            icon: ""
+            icon: "activity/climbing.png"
           },
           {
             activityId: uuid4(),
             label_en: "Camping",
             label_vi: "Cắm trại",
-            icon: ""
+            icon: "activity/night-camping.png"
           },
           {
             activityId: uuid4(),
             label_en: "Swimming",
             label_vi: "Bơi",
-            icon: ""
+            icon: "activity/swimming.png"
           },
           {
             activityId: uuid4(),
             label_en: "Extreme sports",
             label_vi: "Thể thao mạo hiểm",
-            icon: ""
+            icon: "activity/extremem_sports.png"
           }, 
           {
             activityId: uuid4(),
             label_en: "Go sightseeing",
             label_vi: "Tham quan",
-            icon: ""
+            icon: "activity/sightseeing.png"
           },  
           {
             activityId: uuid4(),
             label_en: "Roller coaster",
             label_vi: "Tàu lượn siêu tốc",
-            icon: ""
+            icon: "activity/roller_coster.png"
           }, 
           {
             activityId: uuid4(),
             label_en: "Picnic",
             label_vi: "Dã ngoại",
-            icon: ""
+            icon: "activity/picnic.png"
           }, 
           {
             activityId: uuid4(),
             label_en: "Photography",
             label_vi: "Chụp hình",
-            icon: ""
+            icon: "activity/photographer.png"
           },          
           {
             activityId: uuid4(),
             label_en: "Fishing",
             label_vi: "Câu cá",
-            icon: ""
+            icon: "activity/fishing-man.png"
           },
           {
             activityId: uuid4(),
             label_en: "Paragliding",
             label_vi: "Dù lượn",
-            icon: ""
+            icon: "activity/Paragliding.png"
           },  
           {
             activityId: uuid4(),
             label_en: "Motor racing",
             label_vi: "Đua xe máy",
-            icon: ""
+            icon: "activity/quad-bike.png"
           },
           {
             activityId: uuid4(),
             label_en: "Cycling",
             label_vi: "Đi xe đạp",
-            icon: ""
+            icon: "activity/bicycle-rider.png"
           },
           {
             activityId: uuid4(),
             label_en: "Kayaking",
             label_vi: "Chèo thuyền Kayak",
-            icon: ""
+            icon: "activity/kayak.png"
           },
           {
             activityId: uuid4(),
             label_en: "Surfing",
             label_vi: "Lướt sóng",
-            icon: ""
+            icon: "activity/surfer-surfing.png"
           },
           {
             activityId: uuid4(),
             label_en: "Horseback riding",
             label_vi: "Cưỡi ngựa",
-            icon: ""
+            icon: "activity/horse-riding.png"
           },           
           {
             activityId: uuid4(),
             label_en: "Listen Music",
             label_vi: "Nghe nhạc",
-            icon: ""
+            icon: "activity/music.png"
           },     
           {
             activityId: uuid4(),
             label_en: "Read book",
             label_vi: "Đọc sách",
-            icon: ""
+            icon: "activity/read-book.png"
           },
           {
             activityId: uuid4(),
             label_en: "Watch movie",
             label_vi: "Xem phim",
-            icon: ""
+            icon: "activity/watching-a-movie.png"
           }
         ];
         activityRepo.insertMany(activities);
