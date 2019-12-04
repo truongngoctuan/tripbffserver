9115e68cf914f953e2699a0a929659723038285c
Reborn <vubao1690@gmail.com>
2019-12-04 21:08:29 +0700

    Update method calculate distance between 2 coordinate

--- apis/trip-api/src/trip.module/_core/services/CommonService.ts
+++ apis/trip-api/src/trip.module/_core/services/CommonService.ts
@@ -1,10 +1,17 @@
 
 export function calculateDistance(lat1: number, lon1: number, lat2: number, lon2: number): number {
-    var p = 0.017453292519943295;    // Math.PI / 180
-    var c = Math.cos;
-    var a = 0.5 - c((lat2 - lat1) * p) / 2 +
-        c(lat1 * p) * c(lat2 * p) *
-        (1 - c((lon2 - lon1) * p)) / 2;
+  var R = 6371*1000; // Radius of the earth in meter
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance
+  return d;
+}
 
-    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+function deg2rad(deg: any) {
+    return deg * (Math.PI/180)
 }
