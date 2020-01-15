7a9fc920b85d6c74acd456f6c8d15410212abf06
TRUONG Ngoc Tuan <tuan.truongngoc@pycogroup.com>
2020-01-15 19:59:02 +0700

    integrate into typescript; remove js files redunrant;

--- apis/infographic2/scripts/paper-jsdom-canvas.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const paper_1 = __importDefault(require("paper"));
-// reference from https://github.com/paperjs/paper-jsdom-canvas/blob/master/index.js
-exports.default = paper_1.default;
