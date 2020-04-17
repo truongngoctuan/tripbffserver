d481e725c7e1459d51763d78fec222b21dbb1b9c
TRUONG Ngoc Tuan <tntuan0712494@gmail.com>
2020-04-17 09:21:09 +0700

    moving components around; reuse components;

--- apis/infographic2/configs/02-new-design/index.ts
+++ apis/infographic2/configs/02-new-design/index.ts
@@ -1,5 +1,7 @@
-import { config01Location, config02Locations, configNLocations } from "./config";
-import { config03Locations } from "./03-locations";
+import { config02Locations } from "./2-locations";
+import { config03Locations } from "./3-locations";
+import { configNLocations } from "./n-locations";
+import { config01Location } from "./1-location";
 
 export const fullNewDesignConfig = {
   1: config01Location,
---
+++ apis/infographic2/configs/02-new-design/n-locations.ts
@@ -0,0 +1,80 @@
+import { InfographicConfig } from "..";
+import {
+  sharedHeader,
+  sharedLocationImage02,
+  sharedLocationImage01,
+  locationDetails,
+} from "./shared-components";
+
+// locationDetails will be able to reuse
+export const sharedNLocations: InfographicConfig.LocationsBlocks = {
+  type: "locations",
+  flex: "column",
+  blocks: [
+    // 1st location
+    {
+      type: "location",
+      height: 550,
+      positioning: {
+        // top: 412,
+        left: 97,
+      },
+      blocks: [
+        sharedLocationImage01,
+        // location details
+        {
+          ...locationDetails,
+          positioning: {
+            top: 100,
+            left: 625,
+          },
+        },
+      ],
+    },
+    //2nd location
+    {
+      type: "location",
+      height: 625,
+      positioning: {
+        // top: 724,
+        left: 630,
+      },
+      blocks: [
+        sharedLocationImage02,
+        // location details
+        {
+          ...locationDetails,
+          positioning: {
+            top: 270,
+            left: -450,
+          },
+        },
+      ],
+    },
+  ],
+};
+
+export const configNLocations: InfographicConfig.TripInfographic = {
+  width: 960,
+  // height: 3000,
+  backgroundColor: "#C0E2E5",
+
+  type: "container",
+  flex: "column",
+  blocks: [
+    {
+      ...sharedHeader,
+      scale: 0.75,
+    },
+    {
+      ...sharedNLocations,
+      scale: 0.75,
+    },
+    {
+      type: "container",
+      height: 300,
+      scale: 0.75,
+      blocks: [],
+    },
+  ],
+};
